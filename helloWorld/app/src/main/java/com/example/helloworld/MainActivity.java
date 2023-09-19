7dd8eca1c253242293a57e12900c59d5994ea814
Hosung <an3735297@naver.com>
2023-09-19 19:06:31 +0900

    hello world : view : text, edit, image, button

--- helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView myImageView = findViewById(R.id.myImageView);
+        myImageView.setImageResource(R.drawable.tree);
+
+        TextView myTextView = findViewById(R.id.myTextView);
+        myTextView.setText("Hello World!");
+
+        EditText myEditText = findViewById(R.id.myEditText);
+
+
+
+        Button myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener(v -> {
+            myTextView.setText(myEditText.getText().toString());
+
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+        });
     }
 }
